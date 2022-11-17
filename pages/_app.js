7a2c861f99453d2c2fac8d54dd877a7d6cbd640c
ildarum21 <ildarum.work@yandex.ru>
2022-11-17 21:23:15 +0400

    Initial project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
-function MyApp({ Component, pageProps }) {
+import 'normalize.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
+
+function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
