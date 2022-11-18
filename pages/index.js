eb37a05a7695c094a100824c144f2973f95f2fe2
ildarum21 <ildarum.work@yandex.ru>
2022-11-18 19:17:27 +0400

    Add material UI

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,43 @@
 import * as React from 'react'
 
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
 import styled from 'styled-components'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Hack</h1>
-    </div>
+    <View>
+      <LoginView>
+        <LoginField required label="Email" variant="outlined" />
+        <LoginField required label="Password" variant="outlined" type="password" />
+        <LoginButton variant="contained">Login</LoginButton>
+      </LoginView>
+    </View>
   )
 }
-// const View = styled.div`
-//   margin-top: 10px;
-//   cursor: pointer;
-//   flex-direction: column;
-//   display: flex;
-// `
+
+const View = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const LoginView = styled.div`
+  padding: 40px;
+  border: 1px solid #1976d2;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const LoginField = styled(TextField)`
+  margin-bottom: 10px;
+`
+
+const LoginButton = styled(Button)`
+  width: 100%;
+`
